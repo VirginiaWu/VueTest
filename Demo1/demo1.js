24f5685aa1f97892a3dc9a54f6dd71df141d8af1
virginia.wu <virginia.wu@trantect.com>
2017-01-24 17:45:43 +0800

    learn basic vue.js

---
+++ Demo1/demo1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by developer on 1/24/17.
+ */
+new Vue({
+    el: "#vue1",
+    data: {
+        message: 'Hello Vue'
+    }
+})
+new Vue({
+    el: "#vue2",
+    data: {
+        message: 'You loaded this page on ' + new Date()
+    }
+})
